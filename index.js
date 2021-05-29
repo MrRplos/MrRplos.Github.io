f84af76f352523c9a6f8eb3e7f04264414f8c73d
MrRplos <84832477+MrRplos@users.noreply.github.com>
2021-05-29 14:32:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ var aNumber = 5;
 var aNumber2 = 13;
 console.log(aNumber + aNumber2 * (aNumber2 - aNumber) + aNumber2 + 0.6 )
 console.log(Numbers);
+
+function numberPod(num) {
+    return num > 0 ? "Postive" : num < 0 ? "Neg" : "Zero"
+}
+
+console.log(numberPod(-5));
+
+let myName = "Omran";
+
+if(myName == "Omran") {
+    console.log('Yep');
+} else {
+    console.log('Nope');
+}
+function play() {
+    var sound = document.getElementById('audio');
+    sound.play();
+}
+function blue() {
+    var blue = document.body;
+    blue.classList.toggle("blue-mode");
+}
